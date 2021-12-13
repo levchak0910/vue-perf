068009b958a598a654fa039b9a5ca30ea70873a6
Sándor Levcsák <levchak.sanek@gmail.com>
2021-12-13 19:50:00 +0200

    refactor script: kill

--- scripts/kill.mjs
+++ scripts/kill.mjs
@@ -4,21 +4,6 @@ import "zx/globals"
 
 import {nuxtPort, vitePort, viteLibPort, fastifyPort} from "../config/urls.mjs"
 
-async function killOnPort (port) {
-  try {
-    const processOutput = await $`lsof -i :${port} | grep ${port}`
-    const processes = processOutput.toString().split("\n")
-    let processIds = processes.map(p => p.split(" ").filter(Boolean)[1]).filter(Boolean)
-    processIds = [...new Set(processIds)]
-    for await (const processId of processIds) {
-      await $`kill -9 ${processId}`
-    }
-  }
-  catch (e) {
-    console.log(`Skipped for port: ${port}`)
-  }
-}
+import { killProcessesOnPorts } from "../functions/kill-processes-on-port.mjs"
 
-[nuxtPort, vitePort, viteLibPort, fastifyPort].forEach(killOnPort)
-
-
+await killProcessesOnPorts([nuxtPort, vitePort, viteLibPort, fastifyPort], {exit: true})
