5575bb2a9b62307a9d40d8802fc0cd3e1f4df73f
Sándor Levcsák <levchak.sanek@gmail.com>
2022-05-24 17:42:57 +0300

    add newest fastify-vite app

---
+++ app/fastify-vite-beta/client/base.vue
@@ -0,0 +1,10 @@
+<template>
+  <router-view v-slot="{ Component }">
+    <Suspense>
+      <component
+        :is="Component"
+        :key="$route.path"
+      />
+    </Suspense>
+  </router-view>
+</template>
